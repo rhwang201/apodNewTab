28a2e6f74bfdb87e2aff958388ffed26638e1743
rhwang <richard.hwang@c3energy.com>
2015-05-06 19:16:45 -0700

    Basic styling; its functional

--- js/apod.js
+++ js/apod.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-  /**
-   * @private
-   */
   var padString = function(val, len) {
     if (val.length === len) {
       return val;
@@ -13,9 +10,6 @@ $(document).ready(function() {
     }
   }
 
-  /**
-   * @private
-   */
   var getDateString = function() {
     var today = new Date(),
         year = today.getFullYear(),
@@ -25,23 +19,61 @@ $(document).ready(function() {
     return year + '-' + padString((month + 1).toString(), 2) + '-' + padString(date.toString(), 2);
   };
 
+  var cacheBase64Image = function(imgUrl) {
+    var img = new Image();
+    img.setAttribute('crossOrigin', 'anonymous');
+    img.src = imgUrl;
+    img.onload = function() {
+      var canvas = document.createElement("canvas");
+      canvas.width = this.width;
+      canvas.height = this.height;
+
+      var ctx = canvas.getContext("2d");
+      ctx.drawImage(this, 0, 0);
+
+      var dataURL = canvas.toDataURL("image/png");
+
+      var encodedImage = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+      localStorage['apodNewTab_image_' + getDateString()] = encodedImage;
+    };
+  }
+
   var setTime = function() {
     var now     = new Date(),
-        hour    = now.getHours(),
-        minute  = now.getMinutes();
+        hour    = padString(now.getHours().toString(), 2),
+        minute  = padString(now.getMinutes().toString(), 2);
     $('#apodTime').text(hour + ':' + minute);
   }
 
-  /**
-   * Main function.
-   */
+  var blinkTime = function() {
+    var now     = new Date(),
+        hour    = padString(now.getHours().toString(), 2),
+        minute  = padString(now.getMinutes().toString(), 2);
+
+    var time = $('#apodTime').text();
+    time = (time.indexOf(':') > -1) ? (hour + ' ' + minute) : (hour + ':' + minute);
+
+    $('#apodTime').text(time);
+  }
+
   var handleApod = function() {
-    var response = xhr.response,
-        imageUrl = response.url,
-        title   = response.title;
+    var response    = xhr.response,
+        imageUrl    = response.url,
+        title       = response.title,
+        explanation = response.explanation;
+
+    render(imageUrl, title);
+
+    cacheBase64Image(imageUrl);
+    localStorage['apodNewTab_title_' + getDateString()] = title;
+    localStorage['apodNewTab_explanation_' + getDateString()] = explanation;
+  }
+
+  var render = function(image, title, isBase64Image) {
+    image = (isBase64Image) ? 'data:image/png;base64,' + image: image;
 
     $(document.body).css({
-      'background-image': 'url("' + imageUrl + '")',
+      'background-image': 'url("' + image + '")',
       'background-repeat': 'no-repeat',
       'background-position': 'center center',
       'background-attachment': 'fixed',
@@ -52,17 +84,22 @@ $(document).ready(function() {
     });
 
     $('#apodTitle').text(title);
+  };
 
-    // TODO rhwang: cache the image
-  }
 
+  /**
+   * Main.
+   */
   setTime();
-  setTimeout(setTime, 60000);
+  setInterval(setTime, 60000);
+  setInterval(blinkTime, 1500);
 
-  var dateString = getDateString(),
-      cachedImage = localStorage['apodNewTab_' + dateString];
+  var dateString        = getDateString(),
+      cachedImage       = localStorage['apodNewTab_image_' + dateString],
+      cachedTitle       = localStorage['apodNewTab_title_' + dateString],
+      cachedDescription = localStorage['apodNewTab_description_' + dateString];
 
-  if (cachedImage === undefined) {
+  if (cachedImage === undefined || cachedTitle === undefined || cachedDescription === undefined) {
     // Clear cache
     for (var key in localStorage) {
       if (localStorage.hasOwnProperty(key) && key.substring(0, 10) === 'apodNewTab') {
@@ -84,5 +121,6 @@ $(document).ready(function() {
 
     xhr.send();
   } else {
+    render(cachedImage, cachedTitle, true);
   }
 });
